5666c277c43f509158a69ac7881471841b03f0b3
Nicholas <40147352+n3ptr@users.noreply.github.com>
2022-06-12 16:27:23 -0500

    testing git commands

--- bot/bot.py
+++ bot/bot.py
@@ -14,7 +14,7 @@ f = open(rank_set_path,'r')
 
 guesses = []
 
-# teaching myself how to use vim to edit a file
+# todo - this function needs to be improved to deal with double letters
 
 for line in f:
     guesses.append(line.strip())
